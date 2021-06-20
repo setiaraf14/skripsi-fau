35800b0e5fcf856d9c4cac2a1803d296ad8a6ae6
setiaraf14 <setia.rafif14@gmail.com>
2021-06-21 00:02:32 +0700

    push homepage

--- resources/views/frontend/layout-frontend/main.blade.php
+++ resources/views/frontend/layout-frontend/main.blade.php
@@ -55,7 +55,17 @@
                 @if (Auth::user()->role_user == 'Staff-Kelurahan' || Auth::user()->role == 'Ketua-RW' || Auth::user()->role == 'Ketua-RT' )
                     <li><a class="btn btn-danger" href="{{ url('backend') }}">Dashboard</a></li>        
                 @else
-                    <li><h4> | Hi {{ Auth::user()->name }} !</h4></li>        
+                    <li><h4> | Hi {{ Auth::user()->name }} !</h4></li>
+                    <li>
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </li>        
                 @endif
             @else
                 <li><a href="{{ route('register') }}">Register</a></li>
